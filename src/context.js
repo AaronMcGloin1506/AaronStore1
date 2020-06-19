e359ecdf214cef95eeae7693b507d1fb88c085bf
AaronMcGloin1506 <amcgloin94@gmail.com>
2020-06-19 14:22:44 +0100

    items adding to cart

--- src/context.js
+++ src/context.js
@@ -13,6 +13,9 @@ class ProductProvider extends Component {
         cart: [],
         modalOpen: false,
         modalProduct: detailProduct, 
+        cartSubTotal:0,
+        cartTax:0,
+        cartTotal:0
     };
 
     componentDidMount() {
@@ -67,6 +70,19 @@ class ProductProvider extends Component {
             return{modalOpen: false}
         })
     }
+    increment=(id)=>{
+        console.log('this is the increment method')
+    }
+    decrement=(id)=>{
+        console.log('this is the decrement method')
+    }
+    removeItem=(id)=>{
+        console.log('this is the remove item method')
+    }
+    clearCart=()=>{
+        console.log('cart is not cleared')
+    }
+
     render() {
         return (
             <ProductContext.Provider value = {{
@@ -74,7 +90,11 @@ class ProductProvider extends Component {
                 handleDetail:this.handleDetail,
                 addToCart: this.addToCart,
                 openModal: this.openModal,
-                closeModal: this.closeModal 
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
               {this.props.children}  
             </ProductContext.Provider>
